aca468cc3e26991a75b0c94af6318e9547e25e9a
saurabhlalsaxena <saurabhlal193@gmail.com>
2025-04-13 20:26:34 +0530

    Updates with Blog Post layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,30 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import remarkGfm from 'remark-gfm';
+import rehypePrismPlus from 'rehype-prism-plus';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true
+    }
+  },
   integrations: [
-    mdx(), 
-    sitemap({
-      filter: (page) => !page.includes("/admin"),
-      changefreq: "weekly",
-      priority: 0.7,
+    mdx({
+      syntaxHighlight: 'shiki',
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypePrismPlus],
+      shikiConfig: {
+        theme: 'github-dark',
+        wrap: true
+      },
+      gfm: true
     }),
+    sitemap(),
     tailwind()
   ],
 });
