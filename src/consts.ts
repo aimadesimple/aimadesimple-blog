d67e2f02deaf29876e6509c05d696d1afbf5bdbb
saurabhlalsaxena <saurabhlal193@gmail.com>
2025-06-10 01:07:00 +0530

    Update consts.ts with new site description and title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'AI Made Simple Blog';
+export const SITE_DESCRIPTION = 'AI Made Simple is your go-to blog for demystifying artificial intelligence. We simplify complex AI concepts into easy-to-understand articles, practical tools, and actionable insights—helping individuals, entrepreneurs, and businesses confidently integrate AI into everyday life. Whether you are a curious beginner or a hands-on builder, our content empowers you to learn, build, and stay ahead in the fast-moving world of AI.';
